5e5c19dc5e8d853bfeb6f754cd68034a334c9994
mohanvelaga <113538966+mohanvelaga@users.noreply.github.com>
2024-01-29 21:09:01 +0530

    7

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import axios from 'axios';
 import Layout from './layout';
-import { useState,useEffect } from 'react';
-
-const baseURL = "https://kest-server.onrender.com";
 
 function App() {
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
+  
   return (
     <div>
       <Layout/>
