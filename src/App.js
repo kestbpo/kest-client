672545cd713203e0a68b3a320fe97b9615f4be2a
mohanvelaga <113538966+mohanvelaga@users.noreply.github.com>
2024-01-29 12:32:54 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-// App.js
 import React from 'react';
-//import ScrollSpy from './scrollspy';
-//import MyForm from './form';
+import axios from 'axios';
 import Layout from './layout';
 
+const baseURL = "https://kest-server.onrender.com";
+
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
   return (
     <div>
       <Layout/>
